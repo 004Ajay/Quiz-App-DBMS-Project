44bde7964fe28070c4e707df8589451bd872cada
Judin Augustin <89446098+Minddigger07@users.noreply.github.com>
2023-01-01 16:44:28 +0530

    Update welcome_page_new.py

--- work files/Judin/welcome_page_new.py
+++ work files/Judin/welcome_page_new.py
@@ -12,7 +12,6 @@ bg = Label(window,bg="white")
 bg.place(x=0,y=0,relwidth=1,relheight=1)
 
 
-
 #texts
 date = "<date>"
 join_date = "<join_date>"
@@ -20,6 +19,10 @@ username = "<username>"
 
 welcome = Label(text="Welcome "+username,font=('Arial',40,'bold'),bg="white")
 welcome.place(x=500,y=70)
+
+
+table_label = Label()#label for the recent gameplays table....
+table_label.place()
 
 join = Label(text="Player since "+join_date,font=('Arial',19,'italic'),bg="white")
 join.place(x=0,y=739)
