348b73c59105b5f9603e1fa7a76ec22ca924659d
Ajay T Shaju <ajaytshaju@gmail.com>
2023-01-04 20:28:48 +0530

    required files remained

--- Final_Last/README.md
+++
@@ -1,3 +0,0 @@
-# ReadMe Started
-
-### run [main.py](https://github.com/004Ajay/DBMS-Project/blob/main/main.py) file to see the magic
--- work files/Noyal/tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-    for widget in add_question_frame.winfo_children(): # To delete alredy exisiting widgets in content_frame. # Not working for some reason, try disabling the button.
-        widget.destroy()
